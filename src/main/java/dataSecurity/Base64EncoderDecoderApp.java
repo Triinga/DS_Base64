3dd4ca1b836824add951e8a9cb70ceb35d2e945d
Tringa Baftiu <97124034+Triinga@users.noreply.github.com>
2023-05-13 14:19:11 +0200

    Update Base64EncoderDecoderApp.java

--- src/main/java/dataSecurity/Base64EncoderDecoderApp.java
+++ src/main/java/dataSecurity/Base64EncoderDecoderApp.java
@@ -1,8 +1,39 @@
 package dataSecurity;
 
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class Base64EncoderDecoderApp {
+
+
+public class Base64EncoderDecoderApp extends Application {
+    private Base64Encode b64e;
+    private Base64Decode b64d;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        b64e = new Base64Encode();
+        b64d = new Base64Decode();
+
+        primaryStage.setTitle("Base64 Encoder/Decoder");
+
+        // Input area
+        Label inputLabel = new Label("Enter text to encode:");
+        TextArea inputTextArea = new TextArea();
+        inputTextArea.setPrefRowCount(3);
+        inputTextArea.setPrefColumnCount(40);
+   
    
       // Encode button
         Button encodeButton = new Button("Encode");
