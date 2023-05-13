ccea208dfd6c535466ccf149f14ad67688da93ec
triinga <97124034+Triinga@users.noreply.github.com>
2023-05-13 14:07:17 +0200

    update1

---
+++ src/main/java/dataSecurity/Base64EncoderDecoderApp.java
@@ -0,0 +1,66 @@
+package dataSecurity;
+
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Base64EncoderDecoderApp extends Application {
+    private Base64Encode b64e;
+    private Base64Decode b64d;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        b64e = new Base64Encode();
+        b64d = new Base64Decode();
+
+        primaryStage.setTitle("Base64 Encoder/Decoder");
+
+        // Input area
+        Label inputLabel = new Label("Enter text to encode:");
+        TextArea inputTextArea = new TextArea();
+        inputTextArea.setPrefRowCount(3);
+        inputTextArea.setPrefColumnCount(40);
+        
+        // Encode button
+        Button encodeButton = new Button("Encode");
+        encodeButton.setOnAction(e -> {
+            String inputText = inputTextArea.getText();
+            String encodedText = b64e.encode(inputText);
+            inputTextArea.setText(encodedText);
+        });
+
+        // Decode button
+        Button decodeButton = new Button("Decode");
+        decodeButton.setOnAction(e -> {
+            String encodedText = inputTextArea.getText();
+            String decodedText = b64d.decode(encodedText);
+            inputTextArea.setText(decodedText);
+        });
+
+        // UI layout
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(10));
+        gridPane.setVgap(10);
+        gridPane.setHgap(10);
+        gridPane.add(inputLabel, 0, 0);
+        gridPane.add(inputTextArea, 0, 1);
+        gridPane.add(encodeButton, 0, 2);
+        gridPane.add(decodeButton, 1, 2);
+
+        VBox root = new VBox(gridPane);
+        Scene scene = new Scene(root, 600, 200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.example.ds_base64 {
     requires javafx.controls;
     requires javafx.fxml;
+    exports dataSecurity;
 
 
     opens com.example.ds_base64 to javafx.fxml;
     exports com.example.ds_base64;
+    opens dataSecurity to javafx.fxml;
+
 }
